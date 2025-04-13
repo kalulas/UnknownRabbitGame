656141f7a1f932ea8e70863b1d627512b44470ee
edwardchen <499485532@qq.com>
2025-04-13 18:05:19 +0800

    feat(framework): debug logging support

--- Assets/Scripts/UnknownRabbitGame/Basic/URGEntry.cs
+++ Assets/Scripts/UnknownRabbitGame/Basic/URGEntry.cs
@@ -1,12 +1,13 @@
 ﻿#region FILE HEADER
+
 // Filename: URGEntry.cs
 // Author: Kalulas
 // Create: 2022-10-31
 // Description:
-// Design ->
-// GPP ->
+
 #endregion
 
+using Framework.Debug;
 using UnityBasedFramework.GameScene;
 using UnityEngine;
 
@@ -14,9 +15,26 @@ namespace UnknownRabbitGame.Basic
 {
     public class URGEntry : Entry
     {
+        private static void RegisterLoggers()
+        {
+            Log.RegisterLogger(Log.LogLevel.Debug, Debug.Log);
+            Log.RegisterLogger(Log.LogLevel.Information, Debug.Log);
+            Log.RegisterLogger(Log.LogLevel.Warning, Debug.LogWarning);
+            Log.RegisterLogger(Log.LogLevel.Error, Debug.LogError);
+
+            Log.RegisterFmtLogger(Log.LogLevel.Debug, Debug.LogFormat);
+            Log.RegisterFmtLogger(Log.LogLevel.Information, Debug.LogFormat);
+            Log.RegisterFmtLogger(Log.LogLevel.Warning, Debug.LogWarningFormat);
+            Log.RegisterFmtLogger(Log.LogLevel.Error, Debug.LogErrorFormat);
+        }
+
+        /// <summary>
+        /// This is the entry of the UnknownRabbitGame
+        /// </summary>
         public override void OnEntryAwake()
         {
-            Debug.Log("[URGEntry.OnEntryAwake]");
+            RegisterLoggers();
+            Log.Debug("[URGEntry] EntryAwake!");
             gameObject.AddComponent<URGEventFunctionCaller>();
         }
     }
